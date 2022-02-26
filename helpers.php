d72736317dc81bd59eefe341bf45422e2033f113
M <m@local>
2022-02-26 12:09:15 +0530

    Intial

---
+++ helpers.php
@@ -0,0 +1,531 @@
+<?php
+// Peanuts
+// License: GPL
+
+
+// 
+// Templates
+// 
+
+function render($template_name, $args=[], $html_container='index.php')
+{
+	$template_path = './' . APP_NAME . '/templates/' . APP_TEMPLATE . '/';
+	$uri = $_GET['uri'];
+
+	extract($args, EXTR_SKIP);
+	include $template_path . $html_container;
+	exit;
+}
+
+
+function render_partial($template_name, $args=[], $return=false)
+{
+	$template_path = './' . APP_NAME . '/templates/' . APP_TEMPLATE . '/';
+	
+	extract($args, EXTR_SKIP);
+
+	if($return) ob_start();
+	
+	include $template_path . $template_name;
+	
+	if($return) {
+		$out = ob_get_contents();
+		ob_end_clean();
+		return $out;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+// 
+// URL helpers
+// 
+
+function urlto_template_asset($uri)
+{
+	return CONFIG_ROOT_URL . APP_NAME . '/templates/' . APP_TEMPLATE . '/assets/' . $uri;
+}
+
+
+function urltoget($uri, $args=[], $arg_separator='&')
+{
+	if(!$args) $args = [];
+
+	$hash = isset($args['__hash']) ? '#' . $args['__hash'] : '';
+	unset($args['__hash']);
+
+	$_args = ['uri' => $uri];
+	$args = array_merge($_args, $args);
+
+	return CONFIG_ROOT_URL . '?' . http_build_query($args, '', $arg_separator) . $hash;
+}
+
+
+function urltopost($uri, $args=[], $arg_separator='&')
+{
+	if(!$args) $args = [];
+	
+	$_args = ['post_uri' => $uri];
+	$args = array_merge($_args, $args);
+
+	return CONFIG_ROOT_URL . '?' . http_build_query($args, '', $arg_separator);
+}
+
+
+function redirectto($uri, $args=[])
+{
+	header('Location: ' . urltoget($uri, $args));
+	exit;
+}
+
+
+function get_404()
+{
+	header("HTTP/1.1 404 Not Found");
+	render('404.php', ['__pagetitle'=>'404']);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// HTML Tag helpers
+// 
+
+function formto($uri, $args=[], $attrs=[])
+{
+	$url = urltopost($uri, $args);
+
+	$attrs_str = '';
+	foreach ($attrs as $key => $value) $attrs_str .= "$key='" . htmlentities($value) . "'";
+
+	return "<form method='post' action='$url' $attrs_str>";
+}
+
+
+function linkto($uri, $html, $args=[], $attrs=[])
+{
+	$url = urltoget($uri, $args, '&amp;');
+	
+	$attrs_str = '';
+	foreach ($attrs as $key => $value) $attrs_str .= "$key='" . htmlentities($value) . "' ";
+
+	return "<a href='$url' $attrs_str>" . htmlentities($html) . "</a>";
+}
+
+
+// Auto htmlentities for safe user input
+function tag($html, $attrs=[], $name='div', $closing=true)
+{
+	if($name != 'input' && $name != 'textarea' && !$html) return;
+
+	foreach ($attrs as $key => $value) $attrs_str .= "$key='" . htmlentities($value) . "' ";
+	
+	$out .= "<$name $attrs_str";
+
+	if($name != 'input'){
+		$out .= ">" . htmlentities($html);
+		if($closing) $out .= "</$name>";
+	} else {
+		$out .= "value='" . htmlentities($html) . "'";
+		$out .= " />";
+	}
+
+	return $out;
+}
+
+
+function tag_table($headers, $data, $attrs=[])
+{
+	foreach ($attrs as $key => $value) $attrs_str .= "$key='" . htmlentities($value) . "' ";
+
+	$out = "<table $attrs_str><thead>\n<tr>";
+	foreach ($headers as $key => $value) {
+		$out .= '<th>' . htmlentities($value) . '</th>';
+	}
+	$out .= "</tr>\n</thead>\n<tbody>\n";
+	foreach ($data as $row_key => $row_value) {
+		$out .= "<tr>\n";
+		foreach ($row_value as $cell_key => $cell_value) {
+			$out .= '<td>' . htmlentities($cell_value) . "</td>\n";
+		}
+		$out .= "</tr>\n";
+	}
+	$out .= "</tbody></table>";
+
+	return $out;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Markdown
+// 
+
+function render_markdown($text, $shortcodes=false)
+{
+	$text = strip_tags($text);
+	
+	// Todo: Optimize, use substr.
+	$lines = explode("\n", $text);
+	foreach ($lines as $i => $line) {
+		if(strlen(trim($line)) == 0) $line = "&nbsp;";
+		
+		// Shortcode
+		if($shortcodes && preg_match("/\[[a-z]+[^\]]*\][^(]/", $line)){
+			$out .= process_shortcodes($line);
+		} else {
+			$out .= "<p>\n$line\n</p>\n";
+		}
+	}
+
+	return $out;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Shortcodes
+// 
+
+// Process all shortcodes using respective functions and replace with return values.
+function process_shortcodes($text)
+{
+	// start with [
+	// \[([a-z]+) = name
+	// ([^\]]*) = args_str
+	// [^\(] = Should not match markdown links
+	if(!preg_match_all("/\[([a-z]+)([^\]]*)\][^(]/", $text, $matches)) return $text;
+
+	$shortcodes_list = shortcodes_list();
+	$shortcodes_matches = [];
+	$shortcode_replacements = [];
+	foreach ($matches[0] as $key => $match) {
+		$full_shortcode = trim($matches[0][$key]);
+		$name = $matches[1][$key];
+		$args_str = $matches[2][$key];
+
+		if(!in_array($name, $shortcodes_list)) continue;
+		
+		$args = [];
+		if(strpos($args_str, '=') === false){
+			$args[0] = $args_str;
+		} else {
+			if(preg_match_all('/([a-z]+)="?([^"]+)"?/', $args_str, $args_matches)){
+				$arg_counts = $args_matches[1] ? array_count_values($args_matches[1]) : [];
+
+				foreach ($args_matches[0] as $arg_key => $arg_value) {
+					$key = $args_matches[1][$arg_key];
+					$value = $args_matches[2][$arg_key];
+					
+					if($arg_counts[$key] > 1) $args[$key][] = $value;
+					else $args[$key] = $value;
+				}
+			}
+		}
+
+		// Todo: Optimize, insert using substr.
+		$shortcodes_matches[] = '/' . preg_quote($full_shortcode) . '/';
+		$shortcode_replacements[] = call_user_func("shortcode_$name", $args);
+	}
+
+	return preg_replace($shortcodes_matches, $shortcode_replacements, $text, 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Flash messages
+// 
+
+function flash_set($html, $in_current_request=false)
+{
+	if($html) {
+		if($in_current_request) $_REQUEST['flash'] = $html;
+		else secure_cookie_set('flash', $html);
+	}
+}
+
+
+function flash_clear()
+{
+	cookie_delete('flash');
+}
+
+
+function filter_set_flash()
+{
+	$flash = secure_cookie_get('flash');
+
+	if($flash){
+		$_REQUEST['flash'] = $flash;
+		cookie_delete('flash');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Secure cookie - Cookie with added authenticity
+// 
+
+
+function secure_cookie_set($name, $value)
+{
+	// <= 1/2 day; may expire at am/pm;
+	$authenticity = md5($name . '%' . $value . '%' . date('y-m-d-a') . '%' . SECURE_HASH);
+
+	// Expires end of session/browser close
+	setcookie($name, "$value%$authenticity", 0, CONFIG_ROOT_URL, '', false, true);
+}
+
+
+function secure_cookie_get($name)
+{
+	if(!isset($_COOKIE[$name])) return false;
+
+	$parts = explode('%', $_COOKIE[$name]);
+	$value = $parts[0];
+	$given_authenticity = $parts[1];
+
+	$authenticity = md5($name . '%' . $value . '%' . date('y-m-d-a') . '%' . SECURE_HASH);
+
+	// Todo: If it fails, check if it matches -6
+	if($given_authenticity != $authenticity) return false;
+
+	return $value;
+}
+
+
+function cookie_delete($name)
+{
+	setcookie($name, '', time() - 3600);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Debug helper
+// 
+
+// Simple debug
+// Remember to remove all debugs
+function __d($exit, ...$args)
+{
+	echo "<pre style='width:94%;margin:1%;padding:2%;background:#fff;border:2px solid #aa0000;'>";
+	foreach($args as $arg) {
+		echo htmlentities(print_r($arg, true));
+		echo "<hr/>";
+	}
+	echo "</pre>";
+	if($exit) exit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Config file helpers
+//
+
+
+// Defines constants CONFIG_NAME from config ini file
+function filter_set_config($filepath)
+{
+	$config = parse_ini_file($filepath);
+
+	foreach ($config as $key => $value) {
+		define('CONFIG_' . $key, $value);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Router
+// 
+
+// Map action names to functions and call current name
+// Max action name 32 chars
+function filter_routes($get_action_names, $post_action_names)
+{
+	if( is_string($_GET['post_uri']) && in_array($_GET['post_uri'], $post_action_names)){
+		return call_user_func( 'post_' . preg_replace("/[^a-zA-Z0-9]/", '_', $_GET['post_uri']));
+	}
+
+	if( is_string($_GET['uri']) && in_array($_GET['uri'], $get_action_names)){
+		return call_user_func( 'get_' . preg_replace("/[^a-zA-Z0-9]/", '_', $_GET['uri']));
+	}
+
+	if( !$_GET['uri'] ){
+		return get_root();
+	}
+
+	return get_404();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// Permitted Params
+// 
+
+
+// Permitted GET, POST, cookie params, with strlen check and typecasting
+// Ex: $get_param_names = [ 'param_name' => int_length ... ]
+function filter_permitted_params($get_param_names, $post_param_names, $cookie_param_names, $get_typecasts, $post_typecasts)
+{
+	_filter_permitted_params_names($_GET, $get_param_names);
+	_filter_permitted_params_names($_POST, $post_param_names);
+	_filter_permitted_params_names($_COOKIE, $cookie_param_names);
+
+	_filter_permitted_params_typecast($_GET, $get_typecasts);
+	_filter_permitted_params_typecast($_POST, $post_typecasts);
+}
+
+
+function _filter_permitted_params_names($input, $permitted_arr)
+{
+	foreach ($input as $key => $value) {
+		if(!array_key_exists($key, $permitted_arr)) unset($input[$key]);
+		else if(isset($input[$key])){
+			if(is_int($permitted_arr[$key]) && strlen($input[$key]) > $permitted_arr[$key]) {
+				get_404();
+			}
+			else if(is_string($permitted_arr[$key]) && !preg_match($permitted_arr[$key], $input[$key])) {
+				get_404();
+			}
+		}
+	}
+}
+
+
+function _filter_permitted_params_typecast($input, $typecast_def_arr)
+{
+	foreach ($typecast_def_arr as $name => $type) {
+		if(is_string($input[$name]))
+		switch ($type) {
+			case 'int': $input[$name] = intval($input[$name]); break;
+			case 'float': $input[$name] = floatval($input[$name]); break;
+			case 'bool': $input[$name] = boolval($input[$name]); break;
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,238 @@
+# PHP Helpers
+
+23 functions for building a PHP application.
+
+License: GPL
+
+Status: Work in progress
+
+
+
+## It supports
+
+1. Templates
+2. URL helpers
+3. HTML Tag helpers
+4. Markdown
+5. Shortcodes
+6. Flash messages
+7. Cookie with added authenticity
+8. Debug helper
+9. Config file helpers
+10. Router
+11. Permitted Params
+
+
+## Note
+* **Not tested**, do not use.
+* Please feel free to implement it yourself.
+
+
+Example application:
+
+```php
+require_once './helpers.php';
+
+define('APP_NAME', 'app');
+define('APP_TEMPLATE', 'default');
+
+// Templates are located in './' . APP_NAME . '/templates/' . APP_TEMPLATE . '/'
+
+filter_permitted_params(
+	// GET params with regex
+	[
+		'uri' => '/^[a-z0-9_-]+$/',
+		'post_uri' => '/^[a-z0-9_-]+$/',
+		'id' => '/^\d+$/'
+	],
+	// POST params with max_length
+	[
+		'title' => 1024,
+		'body' => 8192
+	],
+	// COOKIE params with max_length
+	[
+		'flash' => 256
+	],
+	// GET typecast
+	[
+		'id' => 'int',
+	],
+	// POST typecast
+	[ ]
+);
+
+filter_set_flash();
+
+// Routes
+filter_routes(
+	// Get uri
+	[ 'new-post', 'posts' ],
+	// Post uri
+	[ 'create-post' ]
+);
+
+
+//
+// Actions (if needed, place in APP_NAME/actions.php)
+//
+
+function get_root()
+{
+	render('hello.php');
+}
+
+function get_new_post()
+{
+	render('new_post.php');
+}
+
+function get_posts()
+{
+	render('posts.php', ['posts' => $posts]);
+}
+
+function post_create_post()
+{
+	redirectto('posts');
+}
+```
+
+
+
+## Available functions
+
+
+### Templates
+
+1. render($template_name, $args=[], $html_container='index.php')
+	* Renders a PHP template with $args extracted as local variables.
+	* Templates are located in `'./' . APP_NAME . '/templates/' . APP_TEMPLATE . '/'`
+	* `$template_name`, `$template_path`, `$uri` ($_GET['uri']), `$args` are additional local variables.
+	* Todo: Supports sub-templates. Override `default` template files in sub-template.
+	* `$html_container` is the container of the template.
+	* Exits after rendering
+
+2. render_partial($template_name, $args=[], $return=false)
+	* Renders a partial template
+	* All functionality of `render` exists here
+	* It does not exit
+	* `$return` can be used to get HTML instead of rendering directly.
+
+
+### URL helpers
+
+1. urlto_template_asset($uri)
+	* Returns URI to asset in `assets` dir.
+	* Location: CONFIG_ROOT_URL . APP_NAME . '/templates/' . APP_TEMPLATE . '/assets/' . $uri
+
+2. urltoget($uri, $args=[], $arg_separator='&')
+	* Returns URL to a get request
+	* Ex: CONFIG_ROOT_URL?uri=example&arg1=1&arg2=2
+	* Set `$args['__hash']` to set hash part of URL
+
+3. urltopost($uri, $args=[], $arg_separator='&')
+	* Returns URL to a post request
+	* Ex: CONFIG_ROOT_URL?post_uri=example&arg1=1&arg2=2
+
+4. redirectto($uri, $args=[])
+	* Send 404 header and exit
+
+5. get_404()
+	* Built-in route action for 404 page.
+	* /templates/APP_TEMPLATE/404.php
+
+
+### HTML Tag helpers
+
+1. formto($uri, $args=[], $attrs=[])
+	* Returns HTML form open tag with method=post.
+	* Arguments are similar to urltopost
+	* $attrs are additional form tag attributes
+
+2. linkto($uri, $html, $args=[], $attrs=[])
+	* Returns HTML link tag
+	* Arguments are similar to urltoget
+	* $attrs are additional link tag attributes
+
+3. tag($html, $attrs=[], $name='div', $closing=true)
+	* Builds and returns a HTML tag
+	* Uses htmlentities for safe user input
+
+4. tag_table($headers, $data, $attrs=[])
+	* Builds and returns a HTML table tag
+	* Uses htmlentities for safe user input
+
+
+### Markdown
+
+1. render_markdown($text, $shortcodes=false)
+	* Returns markdown as HTML
+
+
+### Shortcodes
+
+1. process_shortcodes($text)
+	* Process shortcodes `[example #1]` through shortcode function.
+	* `shortcodes_list()` must return array of available shortcodes.
+
+
+### Flash messages
+
+1. flash_set($html, $in_current_request=false)
+	* Sets a secure cookie with flash message for use in next request.
+	* Removed in the next request in `filter_set_flash`
+	* `$in_current_request` will make the message available in current request, not next request.
+	* Message is available in `$_REQUEST['flash']`
+
+2. flash_clear()
+	* Clears flash cookie
+
+3. filter_set_flash()
+	* Sets flash message in `$_REQUEST['flash']`, if flash cookie is preset
+	* Deletes flash cookie
+
+
+### Cookie with added authenticity
+
+1. secure_cookie_set($name, $value)
+	* Sets a cookie with built-in authenticity token
+	* Value format: `$value%$authenticity`
+	* Uses md5 hashing (**less secure**)
+
+2. secure_cookie_get($name)
+	* Returns the value, if built-in authenticity is valid
+
+3. cookie_delete($name)
+	* Deletes a cookie
+
+
+### Debug helper
+
+1. __d($exit, ...$args)
+	* Debug variables; uses print_r.
+
+
+### Config file helpers
+
+1. filter_set_config($filepath)
+	* Sets ini file data in `CONFIG_property_name` constants.
+
+
+### Router
+
+1. filter_routes($get_action_names, $post_action_names)
+	* Finds the current `$_GET['uri']` or `$_GET['post_uri']`
+	* Calls `get_$uri` or `get_$post_uri` action functions.
+	* If `$_GET['uri']` is empty, without `post_uri`, calls `get_root`
+	* If nothing matches, calls built-in `get_404`
+
+
+### Permitted Params
+
+1. filter_permitted_params($get_param_names, $post_param_names, $cookie_param_names, $get_typecasts, $post_typecasts)
+	* Remove `$_GET` and `$_POST` params that are not in $get_param_names and $post_param_names.
+	* `$get_param_names` key value is key: param_name, value is either number (strlen) or regex.
+	* `typecasts` change the type of value to specified type.
+
+
