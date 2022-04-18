89b6aedfa2bb99363f0e60f222896501deaf53f0
M <m@local>
2022-04-18 21:56:49 +0530

    cli example

---
+++ apps/example/cli.php
@@ -0,0 +1,69 @@
+<?php
+// 
+// Example of a cli application
+// 
+// This can be used for writing CLI applications or background jobs.
+// 
+// Usage: php cli.php [args]
+// Args will be available in $_GET
+//
+
+define('CUSTOM_GET_404', true);
+
+// See PHP getopt for more options
+// $argc / $argv may also be used, but will not filter for required params in filter_routes
+// a = action name
+$_GET = getopt('a:', ['id:']);
+
+require '../../lib/helpers.php';
+
+
+function initialize(){
+	// Routes
+	$response = filter_routes(
+		[
+			'root'		=> [],
+			'posts'		=> [],
+			'post'		=> ['id']
+		]
+	);
+
+	echo $response === false ? get_404('Route not found.') : $response;
+}
+initialize();
+
+
+// php cli.php
+function get_root()
+{
+	return "Hello from CLI";
+}
+
+
+// php cli.php -a posts
+function get_posts()
+{
+	return "List of all Posts.";
+}
+
+
+// php cli.php -a post --id 1
+function get_post()
+{
+	return "Post #" . $_GET['id'];
+}
+
+
+
+// 
+// Internal
+// 
+
+// php cli.php -a 404
+function get_404($message='')
+{
+	return $message; 
+}
+
+?>
+
